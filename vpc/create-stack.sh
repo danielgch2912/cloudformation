3e075322717a941b45e4780ba83efffc1adcc225
Daniel <danielgch@gmail.com>
2023-01-24 12:21:30 -0300

    Vpc with cloudformation

---
+++ vpc/create-stack.sh
@@ -0,0 +1,2 @@
+aws s3api put-object --bucket cloudformation.awsjava.click --key vpc-create.yml --body vpc-create.yml
+aws cloudformation create-stack --stack-name MyVpc --template-url https://s3.amazonaws.com/cloudformation.awsjava.click/vpc-create.yml
